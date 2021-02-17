c68af0d15c8d394c3728743a5c66f2a4c1cb38b2
SostenesJordan <74992868+SostenesJordan@users.noreply.github.com>
2021-02-17 10:02:34 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Calculadora_de_notas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.calcular = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.TextBox();
+            this.n2 = new System.Windows.Forms.TextBox();
+            this.n1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "N2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "N3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(274, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Digite as suas para notas para ober a média";
+            // 
+            // calcular
+            // 
+            this.calcular.Location = new System.Drawing.Point(111, 124);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(75, 23);
+            this.calcular.TabIndex = 7;
+            this.calcular.Text = "Calcular";
+            this.calcular.UseVisualStyleBackColor = true;
+            this.calcular.Click += new System.EventHandler(this.calcular_Click);
+            // 
+            // n3
+            // 
+            this.n3.Location = new System.Drawing.Point(229, 83);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(38, 20);
+            this.n3.TabIndex = 5;
+            this.n3.TextChanged += new System.EventHandler(this.n3_TextChanged);
+            // 
+            // n2
+            // 
+            this.n2.Location = new System.Drawing.Point(135, 83);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(38, 20);
+            this.n2.TabIndex = 4;
+            this.n2.TextChanged += new System.EventHandler(this.n2_TextChanged);
+            // 
+            // n1
+            // 
+            this.n1.Location = new System.Drawing.Point(39, 83);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(38, 20);
+            this.n1.TabIndex = 3;
+            this.n1.TextChanged += new System.EventHandler(this.n1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(299, 168);
+            this.Controls.Add(this.calcular);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de Notas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button calcular;
+        private System.Windows.Forms.TextBox n3;
+        private System.Windows.Forms.TextBox n2;
+        private System.Windows.Forms.TextBox n1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Calculadora_de_notas
+{
+    public partial class Form1 : Form
+    {
+        double v1, v2, v3;
+        double nota;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void n1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+            v1 = Convert.ToDouble(n1.Text);
+
+            
+        }
+       
+        private void n2_TextChanged(object sender, EventArgs e)
+        {
+            v2 = Convert.ToDouble(n2.Text);
+
+        }
+
+        private void n3_TextChanged(object sender, EventArgs e)
+        {
+            
+            v3 = Convert.ToDouble(n3.Text);
+        }
+
+        private void calcular_Click(object sender, EventArgs e)
+        {
+            nota = (v1 + v2 + v3) / 3;
+
+            if (nota >= 6)
+            {
+                MessageBox.Show("APROVADO! "+
+                "\nsua nota :" + nota);
+
+            }
+            else
+            {
+                MessageBox.Show("REPROVADO!"
+                +"\nsua nota :" + nota);
+
+            }
+
+           
+
+
+        }
+    }
+}
